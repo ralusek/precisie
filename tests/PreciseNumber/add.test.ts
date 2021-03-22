7ca7ae0a90aa3e7503305bbb617f696695a087d4
Tomas Savigliano <ralusek@gmail.com>
2021-03-21 21:44:11 -0600

    added additional tests.

--- tests/PreciseNumber/add.test.ts
+++ tests/PreciseNumber/add.test.ts
@@ -34,5 +34,24 @@ describe('Invocation', () => {
     });
     expect(result.toString({ magnitude: 2 })).to.equal('623');
     expect(result.toString({ magnitude: 0 })).to.equal('6.23');
+
+    a = new PreciseNumber('10');
+    result = a.add(new PreciseNumber('-4'));
+    expect(result).to.nested.include({
+      value: BigInt(6),
+      magnitude: 0,
+    });
+    expect(result.toString({ magnitude: 2 })).to.equal('600');
+    expect(result.toString({ magnitude: 0 })).to.equal('6');
+    expect(result.toString({ magnitude: 0, minPrecision: 2 })).to.equal('6.00');
+
+    a = new PreciseNumber('10');
+    result = a.add(new PreciseNumber('-4.23'));
+    expect(result).to.nested.include({
+      value: BigInt(577),
+      magnitude: 2,
+    });
+    expect(result.toString({ magnitude: 2 })).to.equal('577');
+    expect(result.toString({ magnitude: 0 })).to.equal('5.77');
   });
 });
